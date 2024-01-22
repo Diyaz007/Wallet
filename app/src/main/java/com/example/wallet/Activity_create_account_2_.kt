2a10b366b9638632b195c5c5cc5d7ed2af31591b
Joscan4676 <112806522+Joscan4676@users.noreply.github.com>
2024-01-22 13:26:25 +0600

    5 стпаниц сделал.

--- app/src/main/java/com/example/wallet/Activity_create_account_2_.kt
+++ app/src/main/java/com/example/wallet/Activity_create_account_2_.kt
@@ -3,9 +3,9 @@ package com.example.wallet
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Activity_create_account_2_ : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_create_account2)
     }
 }
---
+++ app/src/main/java/com/example/wallet/Activity_profile_Page_daniyar.kt
@@ -0,0 +1,11 @@
+package com.example.wallet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_profile_Page_daniyar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_page_daniyar)
+    }
+}
