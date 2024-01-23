020da31c393711496217796900f9b6d67b8525cb
Demiurg20 <islamasylbekov1012@gmail.com>
2024-01-22 23:21:57 -0300

    Islam

--- app/src/main/java/com/example/wallet/buy_activity_islam.kt
+++ app/src/main/java/com/example/wallet/buy_activity_islam.kt
@@ -1,11 +1,18 @@
 package com.example.wallet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class buy_activity_islam : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_buy_islam)
+    }
+
+    fun onClickGoMain(view : View) {
+        val intent = Intent(this, convert_islam::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/wallet/convert_islam.kt
@@ -0,0 +1,11 @@
+package com.example.wallet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class convert_islam : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_convert_islam)
+    }
+}
