72ce3bbc6827eb21fca253ede86368d731d2eeda
Demiurg20 <islamasylbekov1012@gmail.com>
2024-01-25 18:33:47 -0300

    Islam

--- app/src/main/java/com/example/wallet/IslamGet.kt
+++ app/src/main/java/com/example/wallet/IslamGet.kt
@@ -11,7 +11,7 @@ class IslamGet : AppCompatActivity() {
         setContentView(R.layout.activity_islam_get)
     }
 
-    fun onClickGoMain(view : View) {
+    fun onClickGoMain(view: View) {
         val intent = Intent(this, IslamSend::class.java)
         startActivity(intent)
     }
