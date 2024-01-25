e358e0ffafec1672a9f0b2c4e999d1af2f11e86f
Demiurg20 <islamasylbekov1012@gmail.com>
2024-01-25 15:32:03 -0300

    Islam

--- app/src/main/java/com/example/wallet/islam_get.kt
+++ app/src/main/java/com/example/wallet/islam_get.kt
@@ -1,11 +1,18 @@
 package com.example.wallet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class islam_get : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_islam_get)
+    }
+
+    fun onClickGoMain(view : View) {
+        val intent = Intent(this, islam_send::class.java)
+        startActivity(intent)
     }
 }
