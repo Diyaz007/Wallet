d8c85f338594ad0d56296048949138648d85b7d8
altyshalu <akylbekovaaltynai30@gmail.com>
2024-01-29 20:34:29 +0600

    password

---
+++ app/src/main/java/com/example/wallet/confirmation_code.kt
@@ -0,0 +1,11 @@
+package com.example.wallet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class confirmation_code : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirmation_code)
+    }
+}
