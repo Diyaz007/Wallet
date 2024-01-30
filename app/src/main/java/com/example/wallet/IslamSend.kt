44d3805028fb1f501e5b3f652b6b3448be854ff6
Demiurg20 <islamasylbekov1012@gmail.com>
2024-01-30 18:01:57 -0300

    .

---
+++ app/src/main/java/com/example/wallet/IslamSend.kt
@@ -0,0 +1,11 @@
+package com.example.wallet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IslamSend : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_islam_send)
+    }
+}
