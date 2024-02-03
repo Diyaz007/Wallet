30364ef3f300ed19fb242e11c3dba11679df2fba
Demiurg20 <islamasylbekov1012@gmail.com>
2024-02-03 10:38:21 -0300

    Islam

--- app/src/main/java/com/example/wallet/ActivityGoBackDaniyar.kt
+++ app/src/main/java/com/example/wallet/ActivityGoBackDaniyar.kt
@@ -1,11 +1,18 @@
 package com.example.wallet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ActivityGoBackDaniyar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_go_back_daniyar)
+    }
+    fun onClickGoProfileActivity(view: View){
+        val intent= Intent(this,ActivityProfilePageDaniyar::class.java)
+        startActivity(intent)
+
     }
 }
