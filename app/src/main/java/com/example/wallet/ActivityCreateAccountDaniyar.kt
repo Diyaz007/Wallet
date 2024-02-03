c0d40d88a83045da07294f87f449b69ef1bb74ed
Demiurg20 <islamasylbekov1012@gmail.com>
2024-02-03 10:33:55 -0300

    Islam

--- app/src/main/java/com/example/wallet/ActivityCreateAccountDaniyar.kt
+++ app/src/main/java/com/example/wallet/ActivityCreateAccountDaniyar.kt
@@ -1,11 +1,17 @@
 package com.example.wallet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ActivityCreateAccountDaniyar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account_daniyar)
+    }
+    fun onClickGoEmail(view: View) {
+        val intent = Intent(this, ActivityCreateAccountSecondDaniyar::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/wallet/ActivityCreateAccountSecondDaniyar.kt
+++ app/src/main/java/com/example/wallet/ActivityCreateAccountSecondDaniyar.kt
@@ -1,11 +1,22 @@
 package com.example.wallet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ActivityCreateAccountSecondDaniyar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account_second_daniyar)
+    }
+    fun onClickGoEmail(view: View) {
+        val intent = Intent(this, ActivityEmailConfirmationDaniyar::class.java)
+        startActivity(intent)
+    }
+    fun onClickGoBackAccCreate(view: View){
+        val intent= Intent(this,ActivityCreateAccountDaniyar::class.java)
+        startActivity(intent)
+
     }
 }
