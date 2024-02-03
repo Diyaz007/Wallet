25b87a88e961b20c149b862211829d49b623f862
Demiurg20 <islamasylbekov1012@gmail.com>
2024-02-03 10:13:58 -0300

    Islam

---
+++ app/src/main/java/com/example/wallet/ActivityCreateAccountSecondDaniyar.kt
@@ -0,0 +1,11 @@
+package com.example.wallet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityCreateAccountSecondDaniyar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account_second_daniyar)
+    }
+}
