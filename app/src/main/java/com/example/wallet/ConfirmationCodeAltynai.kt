95b77c497727f05af1fa58a736400bf1f03e4db7
Demiurg20 <islamasylbekov1012@gmail.com>
2024-02-02 23:03:03 -0300

    Islam

---
+++ app/src/main/java/com/example/wallet/ConfirmationCodeAltynai.kt
@@ -0,0 +1,11 @@
+package com.example.wallet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConfirmationCodeAltynai : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirmation_code_altynai)
+    }
+}
