ae5c06e9abce1fdedae2741debdbe9fb0610849c
Demiurg20 <islamasylbekov1012@gmail.com>
2024-02-03 10:39:28 -0300

    Islam

--- app/src/main/java/com/example/wallet/ActivityProfilePageDaniyar.kt
+++ app/src/main/java/com/example/wallet/ActivityProfilePageDaniyar.kt
@@ -1,11 +1,24 @@
 package com.example.wallet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ActivityProfilePageDaniyar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_page_daniyar)
+    }
+    fun onClickGoAddPaymentActivity(view: View){
+        val intent= Intent(this,ActivityAddPaymentMethodDaniyar::class.java)
+        startActivity(intent)
+
+    }
+
+    fun onClickGoBackActivity(view: View){
+        val intent= Intent(this,ActivityGoBackDaniyar::class.java)
+        startActivity(intent)
+
     }
 }
