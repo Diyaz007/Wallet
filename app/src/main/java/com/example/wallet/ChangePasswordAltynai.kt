2704996335a1a23882e0c446683ac66ae0358365
Demiurg20 <islamasylbekov1012@gmail.com>
2024-02-02 23:22:45 -0300

    Islam

--- app/src/main/java/com/example/wallet/ChangePasswordAltynai.kt
+++ app/src/main/java/com/example/wallet/ChangePasswordAltynai.kt
@@ -1,11 +1,17 @@
 package com.example.wallet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ChangePasswordAltynai : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_change_password_altynai)
+    }
+    fun onClickGoMainAlt4(view : View) {
+        val intent = Intent(this, IslamConvert::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/wallet/ConfirmationCodeAltynai.kt
+++ app/src/main/java/com/example/wallet/ConfirmationCodeAltynai.kt
@@ -1,11 +1,17 @@
 package com.example.wallet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ConfirmationCodeAltynai : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_confirmation_code_altynai)
+    }
+    fun onClickGoMainAlt3(view : View) {
+        val intent = Intent(this, IslamConvert::class.java)
+        startActivity(intent)
     }
 }
