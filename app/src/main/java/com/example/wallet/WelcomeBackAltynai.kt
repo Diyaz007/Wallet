c2ec6c1d359e2dff8ce2ac0893ffec77bac7bb84
Demiurg20 <islamasylbekov1012@gmail.com>
2024-02-02 22:52:30 -0300

    Islam

---
+++ app/src/main/java/com/example/wallet/WelcomeBackAltynai.kt
@@ -0,0 +1,11 @@
+package com.example.wallet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomeBackAltynai : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_back_altynai)
+    }
+}
