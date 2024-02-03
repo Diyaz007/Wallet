f8d75167e1452cd5a4001579b2a9f419e3d7c54f
Demiurg20 <islamasylbekov1012@gmail.com>
2024-02-03 10:19:11 -0300

    Islam

---
+++ app/src/main/java/com/example/wallet/ActivityGoBackDaniyar.kt
@@ -0,0 +1,11 @@
+package com.example.wallet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityGoBackDaniyar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_go_back_daniyar)
+    }
+}
