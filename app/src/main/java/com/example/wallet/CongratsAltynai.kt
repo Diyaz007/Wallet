197a957213e2e13597914603ca52cc8935443d0c
Demiurg20 <islamasylbekov1012@gmail.com>
2024-02-03 15:16:22 -0300

    Islam

--- app/src/main/java/com/example/wallet/CongratsAltynai.kt
+++ app/src/main/java/com/example/wallet/CongratsAltynai.kt
@@ -1,11 +1,17 @@
 package com.example.wallet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class CongratsAltynai : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_congrats_altynai)
+    }
+    fun onClickGoMainAlt5(view : View) {
+        val intent = Intent(this, ActivityCreateAccountDaniyar::class.java)
+        startActivity(intent)
     }
 }
