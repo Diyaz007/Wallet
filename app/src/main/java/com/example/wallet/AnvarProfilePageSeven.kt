7dd41077d72fe5d5cd9ac9d93ed5fb68d574a3ed
Demiurg20 <islamasylbekov1012@gmail.com>
2024-02-04 14:03:16 -0300

    Islam

--- app/src/main/java/com/example/wallet/AnvarProfilePageSeven.kt
+++ app/src/main/java/com/example/wallet/AnvarProfilePageSeven.kt
@@ -1,11 +1,25 @@
 package com.example.wallet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class AnvarProfilePageSeven : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_anvar_profile_page_seven)
     }
+    fun onClickGoBackToProfile5FromProfile7(view : View) {
+        val intent = Intent(this,AnvarProfilePageFive::class.java)
+        startActivity(intent)
+    }
+
+    fun onClickGoToMainActivityAnvar(view : View) {
+        val intent = Intent(this,AnvarCryptoMainActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onClickGoProfileSixAnvar(view: View) {}
+    fun onClickGoProfileSevenAnvar(view: View) {}
 }
--- app/src/main/java/com/example/wallet/AnvarProfilePageSix.kt
+++ app/src/main/java/com/example/wallet/AnvarProfilePageSix.kt
@@ -1,11 +1,17 @@
 package com.example.wallet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class AnvarProfilePageSix : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_anvar_profile_page_six)
+    }
+    fun onClickGoBackToProfile5(view : View) {
+        val intent = Intent(this,AnvarProfilePageFive::class.java)
+        startActivity(intent)
     }
 }
