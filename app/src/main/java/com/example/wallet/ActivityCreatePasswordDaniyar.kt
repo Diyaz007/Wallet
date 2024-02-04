1d9f4a1708b9a73249d14f3ef4927ce14821e928
Demiurg20 <islamasylbekov1012@gmail.com>
2024-02-04 15:54:04 -0300

    Islam

---
+++ app/src/main/java/com/example/wallet/ActivityCreatePasswordDaniyar.kt
@@ -0,0 +1,11 @@
+package com.example.wallet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityCreatePasswordDaniyar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_password_daniyar)
+    }
+}
