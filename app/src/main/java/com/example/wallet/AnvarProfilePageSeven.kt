4743fcee08d7a0cef4b15ad8eb2b4f1891c2ed9c
Demiurg20 <islamasylbekov1012@gmail.com>
2024-02-04 13:48:32 -0300

    Islam

---
+++ app/src/main/java/com/example/wallet/AnvarProfilePageSeven.kt
@@ -0,0 +1,11 @@
+package com.example.wallet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AnvarProfilePageSeven : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_anvar_profile_page_seven)
+    }
+}
