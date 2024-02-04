dc966c4167f2b9743eb08ea4500ede409711e38d
Demiurg20 <islamasylbekov1012@gmail.com>
2024-02-04 14:05:52 -0300

    Islam

---
+++ app/src/main/java/com/example/wallet/AnvarCryptoMainActivity3.kt
@@ -0,0 +1,11 @@
+package com.example.wallet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AnvarCryptoMainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_anvar_crypto_main3)
+    }
+}
---
+++ app/src/main/java/com/example/wallet/AnvarCryptoMainActivity4.kt
@@ -0,0 +1,11 @@
+package com.example.wallet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AnvarCryptoMainActivity4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_anvar_crypto_main4)
+    }
+}
