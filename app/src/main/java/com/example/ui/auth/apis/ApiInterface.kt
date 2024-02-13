bd93a19a8b6ad16ccc33e24b1e2ffa5c2817ffd1
Demiurg20 <islamasylbekov1012@gmail.com>
2024-02-13 17:00:15 -0300

    Islam

--- app/src/main/java/com/example/ui/auth/apis/ApiInterface.kt
+++ app/src/main/java/com/example/ui/auth/apis/ApiInterface.kt
@@ -1,4 +1,10 @@
 package com.example.ui.auth.apis
 
+import com.example.ui.auth.models.MarketModel
+import okhttp3.Response
+import retrofit2.http.GET
 interface ApiInterface {
+
+    @GET("data-api/v3/cryptocurrency/listing?start=1&limit=500")
+    suspend fun getMarketData() : Response
 }
