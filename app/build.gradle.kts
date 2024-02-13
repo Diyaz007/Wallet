2358774781688485bbf1d92856f404baa54f0449
Demiurg20 <islamasylbekov1012@gmail.com>
2024-02-13 15:23:19 -0300

    Islam

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.example.wallet"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -37,13 +37,19 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("android.core:core-ktx:1.12.0")
+    implementation("android.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.retrofit2:adapter-rxjava:2.9.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.androidx.support:design:28.0.0")
+    implementation("com.squareup.picasso:picasso:2.71828")
+    implementation("com.growingio.android:okhttp3:4.1.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.androidx.support:support-annotations:28.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
